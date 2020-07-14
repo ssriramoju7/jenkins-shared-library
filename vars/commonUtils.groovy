2970d7afe6f54444d6a2d692dcb795897557aeaa
Srikanth Sriramoju <ms_ssriramoju@idcontractors.net>
2020-07-14 12:34:15 -0400

    updated

--- vars/commonUtils.groovy
+++ vars/commonUtils.groovy
@@ -3,3 +3,9 @@ def deployApplication(){
 println('Deploy Sucessfully')
 
 }
+
+def testApp(a, b){
+
+	println("a value is : " + a)
+	println("b value is : " + b)
+}
---
+++ vars/devDeploy.groovy
@@ -0,0 +1,15 @@
+node{
+    
+    stage("test") {
+        
+        def abc = commonUtils.deployApplication()
+        println('Sucess')
+    }
+
+    stage('test1') {
+
+    	def a = 1
+    	def b = 2
+    	def abc =commonUtils.testApp(a, b)
+    }
+}
